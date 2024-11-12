e7248686f6c094b489fd87433ff3377217466b9f
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2024-11-12 11:01:36 -0800

    Update EntityTrackerEntryMixin.java

--- src/main/java/survivalblock/rods_from_god/mixin/tungstenrod/EntityTrackerEntryMixin.java
+++ src/main/java/survivalblock/rods_from_god/mixin/tungstenrod/EntityTrackerEntryMixin.java
@@ -1,5 +1,7 @@
 package survivalblock.rods_from_god.mixin.tungstenrod;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
@@ -8,14 +10,10 @@ import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.EntityAttributesS2CPacket;
 import net.minecraft.server.network.EntityTrackerEntry;
 import net.minecraft.server.network.ServerPlayerEntity;
-import org.objectweb.asm.Opcodes;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Constant;
 import survivalblock.rods_from_god.common.entity.TungstenRodEntity;
 
 import java.util.Collection;
@@ -26,13 +24,14 @@ public class EntityTrackerEntryMixin {
 
     @Shadow @Final private Entity entity;
 
-    @Inject(method = "sendPackets", at = @At(value = "FIELD", target = "Lnet/minecraft/server/network/EntityTrackerEntry;alwaysUpdateVelocity:Z", opcode = Opcodes.GETFIELD))
-    private void updateTungstenRodAttributes(ServerPlayerEntity player, Consumer<Packet<ClientPlayPacketListener>> sender, CallbackInfo ci) {
+    @WrapOperation(method = "sendPackets", constant = @Constant(classValue = LivingEntity.class, ordinal = 0))
+    private boolean updateTungstenRodAttributes(Object object, Operation<Boolean> original, ServerPlayerEntity player, Consumer<Packet<ClientPlayPacketListener>> sender) {
         if (this.entity instanceof TungstenRodEntity tungstenRod) {
             Collection<EntityAttributeInstance> collection = tungstenRod.getAttributes().getAttributesToSend();
             if (!collection.isEmpty()) {
                 sender.accept(new EntityAttributesS2CPacket(this.entity.getId(), collection));
             }
         }
+        return false;
     }
 }
