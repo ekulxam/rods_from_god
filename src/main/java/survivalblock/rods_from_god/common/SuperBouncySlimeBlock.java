7c96da6524fe84462104b09dbbdac729c2ff98e5
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2024-12-10 21:03:28 -0800

    Rewrite :)

--- src/main/java/survivalblock/rods_from_god/common/SuperBouncySlimeBlock.java
+++
@@ -1,9 +0,0 @@
-package survivalblock.rods_from_god.common;
-
-import net.minecraft.block.SlimeBlock;
-
-public class SuperBouncySlimeBlock extends SlimeBlock {
-    public SuperBouncySlimeBlock(Settings settings) {
-        super(settings);
-    }
-}
